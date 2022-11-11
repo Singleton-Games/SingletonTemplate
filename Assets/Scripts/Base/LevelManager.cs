be6160b55aa9915dcbbe791cde710881ac5f161d
Selim Ozcan <65914446+selimozcann@users.noreply.github.com>
2022-11-11 17:38:02 +0300

    Update LevelManager.cs

--- Assets/Scripts/Base/LevelManager.cs
+++ Assets/Scripts/Base/LevelManager.cs
@@ -1,28 +1,34 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class LevelManager : Singleton<LevelManager>
 {
-    [Header("Levels")] [SerializeField] private GameObject[] levels;
-    [SerializeField] private int levelIndex;
-    [SerializeField] private Transform levelParent;
+    [Header("Levels")] 
+    [SerializeField] private Level[] levels;
+    [SerializeField] private Level testLevel;
+    public int levelIndex;
 
     private void Awake()
     {
-        LevelSpawn();
+        SetLevelIndex();
+        LevelInit();
     }
-    private void LevelSpawn()
+    private void LevelInit()
+    {
+        Level levelObject = testLevel != null ? SpawnLevel(testLevel) : SpawnLevel(levels[levelIndex]);
+    }
+    private void SetLevelIndex()
     {
         levelIndex = PlayerPrefs.GetInt(StringData.PLAYER);
-
-        if (levels[levelIndex] == null)
-        {
-            Debug.LogError("Level is not found");
-        }
-        else
-        {
-            GameObject levelObject = Instantiate(levels[levelIndex], Vector3.zero, Quaternion.identity, levelParent);
-        }
+        levelIndex = levelIndex == 3 ? 0 : levelIndex;
+        CurrentLevel();
     }
-    public void NextLevel() => PlayerPrefs.SetInt(StringData.PLAYER, levelIndex + 1);
-    public void RestartLevel() => PlayerPrefs.SetInt(StringData.PLAYER, levelIndex);
+    private void CurrentLevel() =>  Debug.Log("Current Level is " + levelIndex + 1);
+    private Level SpawnLevel(Level level) => Instantiate(level,level.transform.position,Quaternion.identity);
+    public void NextLevelData() => PlayerPrefs.SetInt(StringData.PLAYER, levelIndex + 1);
+    public void RestartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+     
 }
+
 
